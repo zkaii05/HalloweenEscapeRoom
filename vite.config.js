3de1b3e91e9909b0b6aec5d8b6332ae7458e742d
zkaii05 <crazymllegends@gmail.com>
2025-08-23 12:43:53 +0800

    updated files for live production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/HalloweenEscapeRoom/', // <-- your repo name
+  base: '/HalloweenEscapeRoom/', // match your repo name
   plugins: [react()],
 });
